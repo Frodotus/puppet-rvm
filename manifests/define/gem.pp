17d854f20f0d49185938f84c4108823e9212c02a
Toni Leino <toni@leino.net>
2011-12-21 15:21:33 +0200

    Gemset added to gem installation

--- manifests/define/gem.pp
+++ manifests/define/gem.pp
@@ -1,6 +1,7 @@
 define rvm::define::gem(
   $ensure = 'present',
   $ruby_version,
+  $gemset = '',
   $gem_version = ''
 ) {  
   ## Set sensible defaults for Exec resource
@@ -12,33 +13,39 @@ define rvm::define::gem(
   $rvm_path = '/usr/local/rvm'
   $rvm_ruby = "${rvm_path}/rubies"
   
+  if $gemset == '' {
+        
+  } else {
+    $rubyset_version = "${ruby_version}@${gemset}"
+  }
   # Setup proper install/uninstall commands based on gem version.
   if $gem_version == '' {
     $gem = {
-      'install'   => "rvm ${ruby_version} gem install ${name} --no-ri --no-rdoc",
-      'uninstall' => "rvm ${ruby_version} gem uninstall ${name}",
+      'install'   => "rvm ${rubyset_version} gem install ${name} --no-ri --no-rdoc",
+      'uninstall' => "rvm ${rubyset_version} gem uninstall ${name}",
       'lookup'    => "rvm gem list | grep ${name}",
     }
   } else {
     $gem = {
-      'install'   => "rvm ${ruby_version} gem install ${name} -v ${gem_version} --no-ri --no-rdoc",
-      'uninstall' => "rvm ${ruby_version} gem uninstall ${name} -v ${gem_version}",
+      'install'   => "rvm ${rubyset_version} gem install ${name} -v ${gem_version} --no-ri --no-rdoc",
+      'uninstall' => "rvm ${rubyset_version} gem uninstall ${name} -v ${gem_version}",
       'lookup'    => "rvm gem list | grep ${name} | grep ${gem_version}",
     }
   }
 
+  
   ## Begin Logic
   if $ensure == 'present' {
     exec { "rvm-gem-install-${name}-${ruby_version}":
       command => $gem['install'],
       unless  => $gem['lookup'],
-      require => [Class['rvm'], Exec["install-ruby-${ruby_version}"]],
+      require => [Class['rvm'], Exec["rvm-gemset-create-${gemset}-${ruby_version}"]],
     }
   } elsif $ensure == 'absent' {
     exec { "rvm-gem-uninstall-${name}-${version}":
       command => $gem['uninstall'],
       onlyif  => $gem['lookup'],
-      require => [Class['rvm'], Exec["install-ruby-${ruby_version}"]],
+      require => [Class['rvm'], Exec["rvm-gemset-create-${gemset}-${ruby_version}"]],
     }    
   }
 }
